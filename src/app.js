e989c1e6d3c8a6175e70d425e43977add3492e1c
Tanvir Islam <tanvir.rb@outlook.com>
2021-08-22 00:56:19 +0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var logger = require('morgan');
+
+var indexRouter = require('./routes');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
